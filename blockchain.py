2b12d9c473a56786b13fae963632840eda70ad1e
Nadim Uddin <nadim.uddin1995@gmail.com>
2018-02-10 13:58:20 -0500

    completed newBlock function

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,35 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        # Create the genesis block(a block with no predecessors)
+        self.new_block(previous_hash=1, proof=100)
         
-    def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
     
     def new_transaction(self, sender, recipient, amount):
         """
@@ -16,7 +40,7 @@ class Blockchain(object):
         :return: <int> The index of the Block that will hold this transaction
         """
 
-        #appends the transaction property of the blockchain
+        # appends the transaction property of the blockchain
         self.current_transactions.append({
             'sender':sender,
             'recipient':recipient,
